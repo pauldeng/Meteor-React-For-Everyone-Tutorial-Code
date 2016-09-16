f60a2706a194d2da7fb4e78e94d5fdb309e60214
Paul Deng <paul.deng@freestyleiot.com>
2016-09-15 23:15:18 -0700

    step 4: Inserting Into Our Database

--- myResolutions/client/App.jsx
+++ myResolutions/client/App.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+Resolutions = new Mongo.Collection("resolutions");
+
 export default class App extends React.Component {
+    addResolution(event){
+        event.preventDefault();
+        let text = this.refs.resolution.value.trim();
+        Resolutions.insert({
+            text: text,
+            complete: false,
+            createdAt: new Date()
+        });
+        this.refs.resolution.value = "";
+        // console.log(text);
+    }
+
     render(){
         return (
-            <h1>Hello World</h1>
+            <div>
+                <h1>Hello World</h1>
+                <form className = "new-resolution" onSubmit={this.addResolution.bind(this)}>
+                <input
+                    type ="text"
+                    ref="resolution"
+                    placeholder="Finish React Meteor Series" />
+                </form>
+            </div>
         )
     }
 }
--- myResolutions/server/main.js
+++ myResolutions/server/main.js
@@ -1,5 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 
+Resolutions = new Mongo.Collection("resolutions");
+
 Meteor.startup(() => {
   // code to run on server at startup
 });
