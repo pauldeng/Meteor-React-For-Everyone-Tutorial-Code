b356f679cf9e90633fc8902c637f5fd7bfec6552
Paul Deng <paul.deng@freestyleiot.com>
2016-09-15 21:15:40 -0700

    step 3: Page Layouts

--- myResolutions/client/App.jsx
+++ myResolutions/client/App.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
+export default class App extends React.Component {
     render(){
         return (
             <h1>Hello World</h1>
         )
     }
 }
-
-Meteor.startup(function(){
-    ReactDOM.render(<App />, document.getElementById("render-target"));
-})
