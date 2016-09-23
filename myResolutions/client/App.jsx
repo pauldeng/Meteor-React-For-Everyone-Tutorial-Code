036b9be26dafb74111dff52c588e76f4dfdac439
Paul Deng <paul.deng@freestyleiot.com>
2016-09-23 00:15:47 -0700

    Step 5: using react-meteor-data

--- myResolutions/client/App.jsx
+++ myResolutions/client/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createContainer } from 'meteor/react-meteor-data';
+
 
 Resolutions = new Mongo.Collection("resolutions");
 
@@ -17,16 +19,27 @@ export default class App extends React.Component {
     }
 
     render(){
-        return (
-            <div>
-                <h1>Hello World</h1>
-                <form className = "new-resolution" onSubmit={this.addResolution.bind(this)}>
-                <input
-                    type ="text"
-                    ref="resolution"
-                    placeholder="Finish React Meteor Series" />
+        let res = this.props.resolutions;
+        if (res.length < 1) {
+            return <div>Loading...</div>
+        } else {
+            return (
+                <div>
+                <h1>My Resolutions</h1>
+                <form className='new-resolution' onSubmit={this.addResolution.bind(this)}>
+                <input type='text' ref={(ref) => this._resolution = ref} placeholder='finish series' />
                 </form>
-            </div>
-        )
+                <div>
+                {res[0].text}
+                </div>
+                </div>
+            );
+        }
     }
 }
+
+export default createContainer(() => {
+  return {
+    'resolutions': Resolutions.find().fetch()
+  };
+}, App);
